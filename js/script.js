6e096f2c824ed6d37f82b1e6bf0cbff6a426cb51
scollo26 <giovanniscollo26@gmail.com>
2021-11-26 16:52:12 +0100

    modifica-html-js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 
-
 const items = [
     'img/01.jpg',
     'img/02.jpg',
@@ -16,90 +15,136 @@ const title = [
     'Paradise'
 ];
 
-
 const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
     'Lorem ipsum',
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ];
 
+const containeritems = document.querySelector('.slider .items');
+const containerthumbs = document.querySelector('.slider .thumbs');
 
-let containerImg = document.querySelector( '.container-img .position-relative');
-
-
-
-
-for(let i = 0; i < items.length; i++){
-    
-    let image = `<img class="" src="${items[i]}" alt="">`;
+for (let i = 0; i < items.length; i++) {
 
     
-    
+    let classElement = '';
+    if (i == 0) {
+        classElement = 'first active';
+    } else if (i == items.length - 1) { 
+        classElement = 'last';
+    }
 
+    const tag = `
+        <div class="item ${classElement}">
+            <img src="${items[i]}" >
+            <div class ="text">
+            <h3>${title[i]}</h3>
+            <p>${text[i]}</p>
+        </div>`;
+    const tagThumb = `
+        <div class="thumb ${classElement}">
+             <img src="${items[i]}">
+        </div>`;
 
-    containerImg.innerHTML += image;
-   
-
-    
-
-}
-
-
-
-let containerImgDestro = document.querySelector( '.col-2');
-
-
-
-for(let i = 0; i < items.length; i++){
-
-    let image = `<div class ="ridimensiona opacity"><img class="" src="${items[i]}" alt=""> </div>`;
-    
-
-
-    containerImgDestro.innerHTML += image;
+    containeritems.innerHTML += tag;
+    containerthumbs.innerHTML += tagThumb;
 }
 
 
 
 
-const imagesBig = document.querySelectorAll('.slider img');
-imagesBig[0].classList.add('active');
-imagesBig[0].classList.add('first');
-imagesBig[imagesBig.length - 1].classList.add('last');
+const buttonNext = document.querySelector('.slider .thumbs .next');
+const buttonPrev = document.querySelector('.slider .thumbs .prev');
+
+
+buttonNext.addEventListener('click',function () {
+    
+        const elementActive = document.querySelector('.slider .items .item.active');
+        const thumbActive = document.querySelector('.slider .thumbs .thumb.active');
+        
+
+       
+        const listClasses = elementActive.classList; 
+        let find = false;
+        for (let i = 0; i < listClasses.length; i++) {
+            if (listClasses[i] == 'last') {
+                find = true;
+                // console.log(listClasses[i]);
+            }
+        }
+
+        
+        
+
+        // controllo-img
+        if (find == false) { 
+            
+            elementActive.classList.remove('active');
+            const elementNext = elementActive.nextElementSibling;
+            elementNext.classList.add('active');
+        }
+
+        let lastThumb = thumbActive.classList.contains('last');
+
+        // controllo-thumbs
+        if (lastThumb == false) {
+            // rimuove-classe
+            thumbActive.classList.remove('active');
+            const thumbNext = thumbActive.nextElementSibling;
+            // aggiunge-classe
+            thumbNext.classList.add('active');
+        }
 
 
 
+    }
+);
+
+
+buttonPrev.addEventListener('click',function () {
+        const elementActive = document.querySelector('.slider .items .item.active');
+        const thumbActive = document.querySelector('.slider .thumbs .thumb.active');
+
+        const listClasses = elementActive.classList; 
+
+        let find = false;
+
+        for (let i = 0; i < listClasses.length; i++) {
+            if (listClasses[i] == 'first') {
+                find = true;
+                console.log(listClasses[i]);
+            }
+        }
+
+        
+
+         // controllo-img
+        if (find == false) { 
+            console.log(elementActive);
+             // rimuove-classe
+            elementActive.classList.remove('active');
+            const elementPrev = elementActive.previousElementSibling;
+
+            console.log(elementPrev);
+
+            // aggiunge-classe
+            elementPrev.classList.add('active');
+        }
+
+        let firstThumb = thumbActive.classList.contains('first');
+        
+        // controllo-thumbs
+        if (firstThumb == false) {
+            // rimuove-classe
+            thumbActive.classList.remove('active');
+            const thumbPrev = thumbActive.previousElementSibling;
+            // aggiunge-classe
+            thumbPrev.classList.add('active');
+        }
 
 
 
-
-
-const buttonUp = document.querySelector('.sopra');
-const buttonDown = document.querySelector('.sotto');
-
-buttonDown.addEventListener('click', function(){
-    let Active = document.querySelector('.slider img.active');
-
-    let imgNext = Active.nextElementSibling;
-    Active.classList.remove('active');
-    imgNext.classList.add('active');
-
-
-});
-
-buttonUp.addEventListener('click', function(){
-    let Active = document.querySelector('.slider img.active');
-
-    let imgNext = Active.previousElementSibling;
-    Active.classList.remove('active');
-    imgNext.classList.add('active');
-});
-
-
-
-
-
-
-
+    }
+);
